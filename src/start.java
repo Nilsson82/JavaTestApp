d93b2a919a67739e906f6b01a7dfd3668d12abb2
Nilsson82 <33781434+Nilsson82@users.noreply.github.com>
2017-11-18 17:53:47 +0100

    HTML

--- src/start.java
+++ src/start.java
@@ -1,10 +1,21 @@
 
 //import static java.lang.System.out;
 
+import java.util.concurrent.Executor;
+import java.util.concurrent.Executors;
+
+
 public class start 
 {
     public static void main  (String args[])
     {
-            System.out.print("Hello Test");
+    		Executor executor = Executors.newSingleThreadExecutor();
+    	
+           Runnable task = () -> System.out.print("Hello Test\n");
+           Runnable task2 = () -> System.out.print("Test\n");
+           
+           executor.execute(task);
+           executor.execute(task2);
+           executor.execute(task);
     }
 }

3b76e95095b1810251828a8aff3c24860f0efcba
Nilsson82 <33781434+Nilsson82@users.noreply.github.com>
2017-11-19 14:03:57 +0100

    Test

---
+++ src/readme.txt
@@ -0,0 +1,12 @@
+ Volume in drive C has no label.
+ Volume Serial Number is 0AFF-5326
+
+ Directory of C:\Users\Anders\Documents\GitHub\JavaTestApp\src
+
+2017-11-19  14:02    <DIR>          .
+2017-11-19  14:02    <DIR>          ..
+2017-11-19  14:02                 0 readme.txt
+2017-11-19  13:44             1ÿ376 start.class
+2017-11-19  13:31               594 start.java
+               3 File(s)          1ÿ970 bytes
+               2 Dir(s)  82ÿ500ÿ808ÿ704 bytes free
--- src/start.java
+++ src/start.java
@@ -9,13 +9,22 @@ public class start
 {
     public static void main  (String args[])
     {
-    		Executor executor = Executors.newSingleThreadExecutor();
+    	Executor executor = Executors.newSingleThreadExecutor();
     	
-           Runnable task = () -> System.out.print("Hello Test\n");
-           Runnable task2 = () -> System.out.print("Test\n");
-           
-           executor.execute(task);
-           executor.execute(task2);
-           executor.execute(task);
+    	Runnable task = () -> System.out.print("Hello Test\n");
+    	Runnable task2 = () -> System.out.print("Test\n");
+       
+    	executor.execute(task);
+    	executor.execute(task2);
+    	test();
+    	executor.execute(task);
+    }
+    
+    
+    public static void test()
+    {
+    	
+    	
+    	
     }
 }
